835d3d474e58d8e2fb880b018a98e1e65345c4f3
ijai <chindaijai49@gmail.com>
2026-06-26 08:21:08 +0000

    new tasks

--- task11.js
+++ task11.js
@@ -47,4 +47,65 @@ let pali = (x) => {
         return false
     }
 }
-console.log(pali(-121))
+// console.log(pali(-121))
+
+let strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+    
+};
+// console.log(strStr("sadbutsad", "sad"))
+
+let mergeTwoLists = function(list1, list2) {
+    // let newList = [];
+    // let join = list1.splice(0, 0, list2)
+    // return newList.push(join)
+    let arr1 = list1
+    let arr2 = arr1.push(list2)
+    let newArr = arr1.flat().sort((a, b) => a - b)
+    return newArr
+};
+
+// console.log(mergeTwoLists([], [0]))
+// function mergeSorted(arr1, arr2){
+//     let arr1 = arr1.splice(arr1.length, 0, ...arr2);
+//         return arr1 = arr2.sort((a, b) => a -b)
+// }
+
+// console.log(mergeSorted([1, 2, 3], [1, 3, 4, 5]))
+
+
+
+function sortedlist(list1, list2){
+   let newArr = list1.splice(list1.length, list1.length, list2)
+    return list1.flat().sort((a, b) => a - b)
+}
+
+// console.log(sortedlist([1, 2, 4],[1, 3, 4, 5]))
+const givenArray = [477, 8914, 40461, 599148];
+const newArray = givenArray.map((n, ind, arr) => ({n , ind, arr}));
+// console.log(newArray); 
+// console.log(givenArray)
+const usCities = [
+  { name: 'New York', population: 8175133 },
+  { name: 'Los Angeles', population: 3792621 },
+  { name: 'Chicago', population: 2695598 },
+  { name: 'Houston', population: 2099451 },
+  { name: 'Philadelphia', population: 1526006 }
+];   
+// const newCity = usCities.filter(({population}) => population >= 2000000)
+let interest = usCities.filter(user => ({...user, population : user.population >= 2000000}));
+
+// console.log(interest)
+let removeElement = function(nums, val) {
+    let newArr = [];
+    for(let i = 0; i < nums.length - 1; i++){
+        if(nums[i] !== val){
+            newArr.push(nums[i])
+
+        }
+    }
+    return newArr
+    
+};
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
+
